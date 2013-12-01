6cc08b19f56bd6d87de2422f37f9a17f2bf60d02
Sungguk Lim <limasdf@gmail.com>
2013-12-01 11:21:17 -0500

    file unittest

--- test/grinder_files_test.dart
+++ test/grinder_files_test.dart
@@ -9,7 +9,7 @@ import 'package:grinder/grinder.dart';
 import 'package:unittest/unittest.dart';
 
 main() {
-  group('grinder FileSet', () {
+  group('grinder.files FileSet', () {
     Directory temp;
     File fileA;
     File fileB;
@@ -62,12 +62,86 @@ main() {
       FileSet fileSetB = new FileSet.fromFile(fileB);
       expect(fileSetB.upToDate(fileSetA), true);
     });
+  });
+
+  group('grinder.files', () {
+    Directory temp;
+    final String sep = Platform.pathSeparator;
+
+    setUp(() {
+      temp = Directory.systemTemp.createTempSync();
+    });
+
+    tearDown(() {
+      temp.deleteSync(recursive: true);
+    });
 
     test('fileName', () {
       final String tempFileName = "temp.txt";
-      final String sep = Platform.pathSeparator;
       File tempFile = new File('${temp.path}${sep}tempdir${sep}${tempFileName}');
       expect(fileName(tempFile), tempFileName);
+    });
+
+    test('fileExt', () {
+      final String extension = 'txt';
+      final String fileName = 'temp' + '.' + extension;
+      File tempFile = new File('${temp.path}${sep}tempdir${sep}${fileName}');
+      expect(fileExt(tempFile), extension);
+
+      final fileNameEmptyExt = 'temp.';
+      tempFile = new File('${temp.path}${sep}tempdir${sep}${fileNameEmptyExt}');
+      expect(fileExt(tempFile), '');
+    });
+
+    test('fileExt null', () {
+      String fileNameNoExt = 'temp';
+      File tempFile =
+        new File('${temp.path}${sep}tempdir${sep}${fileNameNoExt}');
+      expect(fileExt(tempFile), null);
+    });
+
+    test('joinFile', () {
+      File tempFile = joinFile(Directory.current, ['dir','test']);
+      File expectedFile =
+        new File('${Directory.current.path}${sep}dir${sep}test');
+      expect(tempFile.path, expectedFile.path);
+    });
+
+    test('joinDir', () {
+      Directory tempDirectory = joinDir(Directory.current, ['dir','test']);
+      Directory expectedDir =
+        new Directory('${Directory.current.path}${sep}dir${sep}test');
+      expect(tempDirectory.path, expectedDir.path);
+    });
+
+    test('copyFile', () {
+      final String tempFileName = "copytest.txt";
+
+      File source = joinFile(temp, ['${tempFileName}']);
+      source.writeAsStringSync('abcdABCD');
+
+      Directory targetDir = joinDir(temp, ['targetDir']);
+      copyFile(source, targetDir);
+
+      File expectedFile = joinFile(targetDir, ['${tempFileName}']);
+      expect(expectedFile.readAsStringSync(), 'abcdABCD');
+    });
+
+    test('copyDirectory', () {
+      Directory sourceDir = joinDir(temp, ['source']);
+      sourceDir.createSync();
+
+      joinFile(sourceDir, ['fileA']).writeAsStringSync('abcd');
+      joinFile(sourceDir, ['fileB']).writeAsStringSync('efgh');
+      joinFile(sourceDir, ['fileC']).writeAsStringSync('1234');
+
+      Directory targetDir = joinDir(temp,['target']);
+      copyDirectory(sourceDir, targetDir);
+
+      String expectedResult = joinFile(targetDir, ['fileA']).readAsStringSync() +
+                              joinFile(targetDir, ['fileB']).readAsStringSync() +
+                              joinFile(targetDir, ['fileC']).readAsStringSync();
+      expect(expectedResult, 'abcdefgh1234');
     });
   });
 }
