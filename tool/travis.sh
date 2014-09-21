383b55d082f2c0225e7660c6fd32bd6af4a164c4
Devon Carew <devoncarew@gmail.com>
2014-09-21 14:51:16 -0700

    add travis testing

---
+++ tool/travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
+# All rights reserved. Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# Fast fail the script on failures.
+set -e
+
+# Get the Dart SDK.
+DART_DIST=dartsdk-linux-x64-release.zip
+curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
+unzip $DART_DIST > /dev/null
+rm $DART_DIST
+export DART_SDK="$PWD/dart-sdk"
+export PATH="$DART_SDK/bin:$PATH"
+
+# Invoke the drone.sh script (which assumes that dart-sdk/bin is on the path).
+./tool/drone.sh
