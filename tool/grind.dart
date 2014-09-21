d043bc0d3e446389b4f5d5960a828b98c9c52b4d
Devon Carew <devoncarew@gmail.com>
2014-09-21 14:31:49 -0700

    tweaks

--- tool/grind.dart
+++ tool/grind.dart
@@ -7,7 +7,6 @@ void main([List<String> args]) {
   task('init', run: init);
   task('analyze', run: analyze, depends: ['init']);
   task('tests', run: tests, depends: ['init']);
-  //task('docs', run: docs, depends: ['analyze']);
 
   startGrinder(args);
 }
@@ -27,18 +26,3 @@ void analyze(GrinderContext context) {
 void tests(GrinderContext context) {
   runDartScript(context, "test/all.dart");
 }
-
-//void docs(GrinderContext context) {
-//  FileSet docFiles = new FileSet.fromDir(
-//      new Directory('docs'), pattern: '*.html');
-//  FileSet sourceFiles = new FileSet.fromDir(
-//      new Directory('lib'), pattern: '*.dart', recurse: true);
-//
-//  if (!docFiles.upToDate(sourceFiles)) {
-//    runSdkBinary(context, 'dartdoc',
-//        arguments: ['--omit-generation-time',
-//                    '--package-root', 'packages/',
-//                    '--include-lib', 'grinder,grinder.files,grinder.utils',
-//                    'lib/grinder.dart']);
-//  }
-//}
