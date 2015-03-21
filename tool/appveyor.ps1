d4225b15eb7fc9ff306c1efee317988f5c09bc9c
guillermooo <guillermo.lopez@outlook.com>
2015-03-21 08:39:42 +0100

    add missing license; refactor

--- tool/appveyor.ps1
+++ tool/appveyor.ps1
@@ -1,8 +1,18 @@
+# Copyright (c) 2014, Google Inc. Please see the AUTHORS file for details.
+# All rights reserved. Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
 param([string]$action)
+
+function throw_if_process_failed {
+    param([string]$message)
+    if ($LASTEXITCODE -ne 0) { throw $message }
+}
 
 function install {
     start-filedownload https://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-windows-x64-release.zip
     7z.exe x dartsdk-windows-x64-release.zip -oc:\ | select-string "^Extracting" -notmatch
+    throw_if_process_failed "could not extract sdk"
 }
 
 function test {
@@ -12,7 +22,7 @@ function test {
     pub global activate tuneup
     # Verify that the libraries are error free.
     pub global run tuneup check --ignore-infos
-    if ($LASTEXITCODE -ne 0) { throw "libraries have errors" }
+    throw_if_process_failed "libraries have errors"
 
     # Run the tests.
     dart test/all.dart 
