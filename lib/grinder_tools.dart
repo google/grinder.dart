4d31c46ee9dc2af41847e5ede03438c197f2f6e4
Devon Carew <devoncarew@gmail.com>
2015-06-10 20:30:15 -0700

    remove the  class implementations

--- lib/grinder_tools.dart
+++ lib/grinder_tools.dart
@@ -6,16 +6,11 @@ library grinder.tools;
 
 import 'dart:async';
 import 'dart:io';
-import 'dart:math' as math;
-
-import 'package:webkit_inspection_protocol/webkit_inspection_protocol.dart';
-import 'package:which/which.dart';
 
 import 'grinder.dart';
 import 'src/run.dart' as run_lib;
 import 'src/run_utils.dart';
 import 'src/utils.dart';
-import 'src/_mserve.dart';
 
 export 'src/run.dart';
 
@@ -58,15 +53,12 @@ void defaultInit([GrinderContext context]) {}
 /// artifacts in the `build/`.
 void defaultClean([GrinderContext context]) => delete(buildDir);
 
-/**
- * A utility class to run tests for your project.
- */
+/// A utility class to run tests for your project.
 @Deprecated('see [TestRunner]')
 class Tests {
-  /**
-   * Run command-line tests. You can specify the base directory (`test`), and
-   * the file to run (`all.dart`).
-   */
+  /// Run command-line tests. You can specify the base directory (`test`), and
+  /// the file to run (`all.dart`).
+   @Deprecated('see [TestRunner]')
   static void runCliTests(
       {String directory: 'test', String testFile: 'all.dart'}) {
     String file = '${directory}/${testFile}';
@@ -74,105 +66,13 @@ class Tests {
     Dart.run(file);
   }
 
-  /**
-   * Run web tests in a browser instance. You can specify the base directory
-   * (`test`), and the html file to run (`index.html`).
-   */
+  /// Run web tests in a browser instance.
+  @Deprecated('see [TestRunner]')
   static Future runWebTests({String directory: 'test',
-      String htmlFile: 'index.html', Chrome browser}) {
-    // Choose a random port to tell the browser to serve debug info to. If we
-    // specify a fixed port the browser may fail to connect, but we'll still try
-    // and create a debug connection to the port.
-    int wip = 33000 + new math.Random().nextInt(10000); //9222;
-
-    if (browser == null) {
-      if (directory.startsWith('build')) {
-        browser = Chrome.getBestInstalledChrome();
-      } else {
-        browser = Chrome.getBestInstalledChrome(preferDartium: true);
-      }
-    }
-
-    if (browser == null) {
-      return new Future.error('Unable to locate a Chrome install');
-    }
-
-    MicroServer server;
-    BrowserInstance browserInstance;
-    String url;
-    ChromeTab tab;
-    WipConnection connection;
-
-    // Start a server.
-    return MicroServer.start(port: 0, path: directory).then((s) {
-      server = s;
-
-      log("microserver serving '${server.path}' on ${server.urlBase}");
-
-      // Start the browser.
-      log('opening ${browser.browserPath}');
-
-      List<String> args = ['--remote-debugging-port=${wip}'];
-      if (Platform.environment['CHROME_ARGS'] != null) {
-        args.addAll(Platform.environment['CHROME_ARGS'].split(' '));
-      }
-      url = 'http://${server.host}:${server.port}/${htmlFile}';
-      return browser.launchUrl(url, args: args);
-    }).then((bi) {
-      browserInstance = bi;
-
-      // Find tab.
-      return new ChromeConnection(server.host, wip).getTab((tab) {
-        return tab.url == url || tab.url.endsWith(htmlFile);
-      }, retryFor: new Duration(seconds: 5));
-    }).then((t) {
-      tab = t;
-
-      log('connected to ${tab}');
-
-      // Connect via WIP.
-      return WipConnection.connect(tab.webSocketDebuggerUrl);
-    }).then((c) {
-      connection = c;
-      connection.console.enable();
-      StreamSubscription sub;
-      ResettableTimer timer;
-
-      var teardown = () {
-        sub.cancel();
-        connection.close();
-        browserInstance.kill();
-        server.destroy();
-        timer.cancel();
-      };
-
-      Completer completer = new Completer();
-
-      timer = new ResettableTimer(new Duration(seconds: 60), () {
-        teardown();
-        if (!completer.isCompleted) {
-          completer.completeError('tests timed out');
-        }
-      });
-
-      sub = connection.console.onMessage.listen((ConsoleMessageEvent event) {
-        timer.reset();
-        log(event.text);
-
-        // 'tests finished - passed' or 'tests finished - failed'.
-        if (event.text.contains('tests finished -')) {
-          teardown();
-
-          if (event.text.contains('tests finished - failed')) {
-            completer.completeError('tests failed');
-          } else {
-            completer.complete();
-          }
-        }
-      });
-
-      return completer.future;
-    });
+      String htmlFile: 'index.html', dynamic browser}) {
+    fail('See the TestRunner class (and the `test` package) in order to run web'
+        ' based tests.');
+    return new Future.value();
   }
 }
 
@@ -252,233 +152,4 @@ class TestRunner {
     // TODO: Pass in --color based on a global property: #243.
     return args;
   }
-}
-
-class Chrome {
-  static Chrome getBestInstalledChrome({bool preferDartium: false}) {
-    Chrome chrome;
-
-    if (preferDartium) {
-      chrome = new Dartium();
-      if (chrome.exists) return chrome;
-    }
-
-    chrome = new Chrome.createChromeStable();
-    if (chrome.exists) return chrome;
-
-    chrome = new Chrome.createChromeDev();
-    if (chrome.exists) return chrome;
-
-    chrome = new Chrome.createChromium();
-    if (chrome.exists) return chrome;
-
-    if (!preferDartium) {
-      chrome = new Dartium();
-      if (chrome.exists) return chrome;
-    }
-
-    return null;
-  }
-
-  final String browserPath;
-  Directory _tempDir;
-
-  Chrome(this.browserPath) {
-    _tempDir = Directory.systemTemp.createTempSync('userDataDir-');
-  }
-
-  Chrome.createChromeStable() : this(_chromeStablePath());
-  Chrome.createChromeDev() : this(_chromeDevPath());
-  Chrome.createChromium() : this(_chromiumPath());
-
-  bool get exists => new File(browserPath).existsSync();
-
-  void launchFile(String filePath, {bool verbose: false,
-      @Deprecated('see RunOptions.environment') Map envVars,
-      run_lib.RunOptions runOptions}) {
-    mergeEnvironment(envVars, runOptions);
-    String url;
-
-    if (new File(filePath).existsSync()) {
-      url = 'file:/' + new Directory(filePath).absolute.path;
-    } else {
-      url = filePath;
-    }
-
-    List<String> args = [
-      '--no-default-browser-check',
-      '--no-first-run',
-      '--user-data-dir=${_tempDir.path}'
-    ];
-
-    if (verbose) {
-      args.addAll(['--enable-logging=stderr', '--v=1']);
-    }
-
-    args.add(url);
-
-    // TODO: This process often won't terminate, so that's a problem.
-    log("starting chrome...");
-    run_lib.run(browserPath, arguments: args, runOptions: runOptions);
-  }
-
-  Future<BrowserInstance> launchUrl(String url, {List<String> args,
-      bool verbose: false,
-      @Deprecated('see RunOptions.environment') Map envVars,
-      run_lib.RunOptions runOptions}) {
-    mergeEnvironment(envVars, runOptions);
-    List<String> _args = [
-      '--no-default-browser-check',
-      '--no-first-run',
-      '--user-data-dir=${_tempDir.path}'
-    ];
-
-    if (verbose) _args.addAll(['--enable-logging=stderr', '--v=1']);
-    if (args != null) _args.addAll(args);
-
-    _args.add(url);
-
-    return Process
-        .start(browserPath, _args,
-            workingDirectory: runOptions.workingDirectory,
-            environment: runOptions.environment,
-            includeParentEnvironment: runOptions.includeParentEnvironment,
-            runInShell: runOptions.runInShell)
-        .then((Process process) {
-      // Handle stdout.
-      var stdoutLines = toLineStream(process.stdout, runOptions.stdoutEncoding);
-      stdoutLines.listen(logStdout);
-
-      // Handle stderr.
-      var stderrLines = toLineStream(process.stderr, runOptions.stderrEncoding);
-      stderrLines.listen(logStderr);
-
-      return new BrowserInstance(this, process);
-    });
-  }
-}
-
-class BrowserInstance {
-  final Chrome browser;
-  final Process process;
-
-  int _exitCode;
-
-  BrowserInstance(this.browser, this.process) {
-    process.exitCode.then((int code) {
-      _exitCode = code;
-    });
-  }
-
-  int get exitCode => _exitCode;
-
-  bool get running => _exitCode != null;
-
-  void kill() {
-    process.kill();
-  }
-}
-
-/**
- * A wrapper around the Dartium browser.
- */
-class Dartium extends Chrome {
-  Dartium() : super(_dartiumPath());
-}
-
-class ContentShell extends Chrome {
-  static String _contentShellPath() {
-    final Map m = {
-      "linux": "content_shell/content_shell",
-      "macos": "content_shell/Content Shell.app/Contents/MacOS/Content Shell",
-      "windows": "content_shell/content_shell.exe"
-    };
-
-    String sep = Platform.pathSeparator;
-    String os = Platform.operatingSystem;
-    String dartSdkPath = sdkDir.path;
-
-    // Truncate any trailing /'s.
-    if (dartSdkPath.endsWith(sep)) {
-      dartSdkPath = dartSdkPath.substring(0, dartSdkPath.length - 1);
-    }
-
-    String path = "${dartSdkPath}${sep}..${sep}chromium${sep}${m[os]}";
-
-    if (FileSystemEntity.isFileSync(path)) {
-      return new File(path).absolute.path;
-    }
-
-    return null;
-  }
-
-  ContentShell() : super(_contentShellPath());
-}
-
-String _dartiumPath() {
-  final Map m = {
-    "linux": "chrome",
-    "macos": "Chromium.app/Contents/MacOS/Chromium",
-    "windows": "chrome.exe"
-  };
-
-  String sep = Platform.pathSeparator;
-  String os = Platform.operatingSystem;
-  String dartSdkPath = sdkDir.path;
-
-  // Truncate any trailing /'s.
-  if (dartSdkPath.endsWith(sep)) {
-    dartSdkPath = dartSdkPath.substring(0, dartSdkPath.length - 1);
-  }
-
-  String path = "${dartSdkPath}${sep}..${sep}chromium${sep}${m[os]}";
-
-  if (FileSystemEntity.isFileSync(path)) {
-    return new File(path).absolute.path;
-  }
-
-  path = whichSync('Dartium', orElse: () => null);
-
-  return path;
-}
-
-String _chromeStablePath() {
-  if (Platform.isLinux) {
-    return '/usr/bin/google-chrome';
-  } else if (Platform.isMacOS) {
-    return '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
-  } else {
-    List paths = [
-      r"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
-      r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-    ];
-
-    for (String path in paths) {
-      if (new File(path).existsSync()) {
-        return path;
-      }
-    }
-  }
-
-  return null;
-}
-
-String _chromeDevPath() {
-  if (Platform.isLinux) {
-    return '/usr/bin/google-chrome-unstable';
-  } else if (Platform.isMacOS) {
-    return '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary';
-  } else {
-    return null;
-  }
-}
-
-String _chromiumPath() {
-  if (Platform.isLinux) {
-    return '/usr/bin/chromium-browser';
-  } else if (Platform.isMacOS) {
-    return '/Applications/Chromium.app/Contents/MacOS/Chromium';
-  }
-
-  return null;
 }
