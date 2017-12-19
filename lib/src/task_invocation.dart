2af3add04599f0fc78f90a808b2f74b675dda937
Devon Carew <devoncarew@google.com>
2017-12-18 17:24:42 -0800

    remove dep on package:collection

--- lib/src/task_invocation.dart
+++ lib/src/task_invocation.dart
@@ -3,8 +3,6 @@
 
 library grinder.src.task_invocation;
 
-import 'package:collection/collection.dart';
-
 /// An invocation of a [GrinderTask].
 ///
 /// Identifies the [name] of the task to invoke, and any [arguments] to send to
@@ -22,11 +20,9 @@ class TaskInvocation {
   bool operator ==(other) =>
       other is TaskInvocation &&
       name == other.name &&
-      const IterableEquality()
-          .equals(arguments.arguments, other.arguments.arguments);
+      _listEquals(arguments.arguments, other.arguments.arguments);
 
-  int get hashCode =>
-      name.hashCode ^ (const IterableEquality().hash(arguments.arguments) * 3);
+  int get hashCode => name.hashCode ^ (arguments.arguments.length * 3);
 
   String toString() {
     var args = arguments.arguments;
@@ -88,5 +84,19 @@ class TaskArgs {
         }
       }
     }
+  }
+}
+
+bool _listEquals(List elements1, List elements2) {
+  if (identical(elements1, elements2)) return true;
+  if (elements1 == null || elements2 == null) return false;
+  if (elements1.length != elements2.length) return false;
+  var it1 = elements1.iterator;
+  var it2 = elements2.iterator;
+  while (true) {
+    bool hasNext = it1.moveNext();
+    if (hasNext != it2.moveNext()) return false;
+    if (!hasNext) return true;
+    if (it1.current != it2.current) return false;
   }
 }
